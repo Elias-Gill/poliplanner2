2dda7fc79ad527701d0a4167f2285ace7ddf527e
Elias Gill <eliasgill42@gmail.com>
2025-12-15 15:21:52 -0300

    feat: adding tools endpoint to the navbar

--- router/misc_router.go
+++ router/misc_router.go
@@ -12,9 +12,15 @@ func NewMiscRouter() func(r chi.Router) {
 	layouts := web.BaseLayout
 
 	return func(r chi.Router) {
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "text/html")
+			tpl := template.Must(template.Must(layouts.Clone()).ParseFiles("web/templates/pages/misc/index.html"))
+			tpl.Execute(w, nil)
+		})
+
 		r.Get("/calculator", func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Content-Type", "text/html")
-			tpl := template.Must(template.Must(layouts.Clone()).ParseFiles("web/templates/pages/calculator.html"))
+			tpl := template.Must(template.Must(layouts.Clone()).ParseFiles("web/templates/pages/misc/calculator.html"))
 			tpl.Execute(w, nil)
 		})
 	}
